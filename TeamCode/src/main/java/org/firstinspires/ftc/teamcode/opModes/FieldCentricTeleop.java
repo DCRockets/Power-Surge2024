9961c26ba83728d77cdebf9edde2bbde797c5079
LWiscool7 <lwambach21@gmail.com>
2024-09-12 18:48:05 -0500

    9/10/2024 meeting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/FieldCentricTeleop.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "FieldCentric")
+public class FieldCentricTeleop extends LinearOpMode {
+    @Override public void runOpMode() {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("frontRight");
+        DcMotor frontRight = hardwareMap.dcMotor.get("backLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+
+        // Reverse the right side motors
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Initialize imu
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            double x = gamepad1.left_stick_x;
+            double y = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+
+            // Read inverse imu heading
+            double botHeading = -imu.getAngularOrientation().firstAngle;
+
+            // Rotate gamepad input x/y by hand
+            // See: https://matthew-brett.github.io/teaching/rotation_2d.html
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+            double frontLeftPower = rotY + rotX + turn;
+            double backLeftPower = rotY - rotX + turn;
+            double frontRightPower = rotY - rotX - turn;
+            double backRightPower = rotY + rotX - turn;
+
+            // Put powers in the range of -1 to 1 only if they aren't already
+            // Not checking would cause us to always drive at full speed
+            if (Math.abs(frontLeftPower) > 1 || Math.abs(backLeftPower) > 1 ||
+                    Math.abs(frontRightPower) > 1 || Math.abs(backRightPower) > 1) {
+                // Find the largest power
+                double max = 0;
+                max = Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower));
+                max = Math.max(Math.abs(frontRightPower), max);
+                max = Math.max(Math.abs(backRightPower), max);
+
+                // Divide everything by max (it's positive so we don't need to worry
+                // about signs)
+                frontLeftPower /= max;
+                backLeftPower /= max;
+                frontRightPower /= max;
+                backRightPower /= max;
+            }
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+        }
+    }
+}
